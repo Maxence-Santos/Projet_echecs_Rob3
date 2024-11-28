5926911bca8f773183110cc4ceb162d45eb3400b
Maxence <maxence1405@free.fr>
2024-11-28 17:43:24 +0100

    modif creer_plateau()

--- affichage.h
+++ affichage.h
@@ -2,6 +2,12 @@
 #include "struct.h"
 #include "creer_plateau.h"
 
-void affichage(Partie* partie) {
-    int plateau** = creerplateau();
+void affichage(Partie partie) {
+    Case** tab = creerplateau();
+    for (int i=0;i<=7;i++) {
+        for (int j=0;j<=7;j++) {
+            printf("%d ",tab[i][j]);
+        }
+        printf("\n");
+    }
 }
--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "creer_plateau.h"
+#include "affichage.h"
 
 int main() {
-    
+    Partie partie;
     Case **tab = creerplateau();
+    partie.plateau = tab;
+    partie.joueur_actif = blanc;
 
-    printf("tableau :  %d\n", tab[0][0].p==tour);
-
+    //printf("tableau :  %d\n", tab[0][0].p==tour);
+    affichage(partie);
 }
